65390bb2ffb74c5b11e7ce6bb8232ad7e479e36a
alinem <158090981+alinemaligorne@users.noreply.github.com>
2024-06-20 16:35:29 +0200

    Update index.js
    Json integration and get

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 //serveur express
 const express = require('express')
 const app = express()
+const parkings = require('./parkings.json')
 app.get('/parkings', (req,res) => {    
-    res.send("Liste des parkings")
+    res.status(200).json(parkings)
 })
+
+app.get('/parkings/:id', (req,res) => {    
+    const id = parseInt(req.params.id)    
+    const parking = parkings.find(parking => parking.id === id)    
+    res.status(200).json(parking)
+})
+
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
 })
