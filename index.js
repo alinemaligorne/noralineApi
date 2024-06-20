9e3b9e234a74c9b2ce0c51ddbfcb41b4aa0492f0
alinem <158090981+alinemaligorne@users.noreply.github.com>
2024-06-20 14:50:52 +0200

    Mise en place de l'espace de travail
    node, index.js, express

---
+++ index.js
@@ -0,0 +1,9 @@
+//serveur express
+const express = require('express')
+const app = express()
+app.get('/parkings', (req,res) => {    
+    res.send("Liste des parkings")
+})
+app.listen(8080, () => {
+    console.log("Serveur à l'écoute")
+})
