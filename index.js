5acf77f0578a8f4e96472be4ad247f9031446fbf
nora <noraidris621@gmail.com>
2024-06-21 16:12:18 +0200

    code a jour

--- index.js
+++ index.js
@@ -19,12 +19,4 @@ app.post('/parkings', (req,res) => {
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")})
 
-// const express = require('express')
-// const app = express()
-// const parkings = require('./parkings.json')
-// Middlewareapp.use(express.json())
-// app.get('/parkings', (req,res) => {
-// res.status(200).json(parkings)})app.get('/parkings/:id', (req,res) => {
-//const id = parseInt(req.params.id) 
-//const parking = parkings.find(parking => parking.id === id)
-//res.status(200).json(parking)})app.listen(8080, () => {console.log("Serveur à l'écoute")})
+    
