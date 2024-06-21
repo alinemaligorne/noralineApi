72ab7637830fa702255372677c5e93cbd5d9d0b3
nora <noraidris621@gmail.com>
2024-06-21 16:08:47 +0200

    mon version

--- index.js
+++ index.js
@@ -2,16 +2,29 @@
 const express = require('express')
 const app = express()
 const parkings = require('./parkings.json')
-app.get('/parkings', (req,res) => {    
+//Middleware
+app.use(express.json())
+app.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
 })
-
-app.get('/parkings/:id', (req,res) => {    
-    const id = parseInt(req.params.id)    
-    const parking = parkings.find(parking => parking.id === id)    
+app.get('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const parking = parkings.find(parking => parking.id === id)
     res.status(200).json(parking)
 })
-
-app.listen(8080, () => {
-    console.log("Serveur à l'écoute")
+app.post('/parkings', (req,res) => {
+    parkings.push(req.body)
+    res.status(200).json(parkings)
 })
+app.listen(8080, () => {
+    console.log("Serveur à l'écoute")})
+
+// const express = require('express')
+// const app = express()
+// const parkings = require('./parkings.json')
+// Middlewareapp.use(express.json())
+// app.get('/parkings', (req,res) => {
+// res.status(200).json(parkings)})app.get('/parkings/:id', (req,res) => {
+//const id = parseInt(req.params.id) 
+//const parking = parkings.find(parking => parking.id === id)
+//res.status(200).json(parking)})app.listen(8080, () => {console.log("Serveur à l'écoute")})
