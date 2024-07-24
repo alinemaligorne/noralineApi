ca08156ef89023c56d972e747d0251a094e33501
alinem <158090981+alinemaligorne@users.noreply.github.com>
2024-07-24 11:41:20 +0200

    update index.js

--- index.js
+++ index.js
@@ -18,5 +18,15 @@ app.post('/parkings', (req,res) => {
     parkings.push(req.body)    
     res.status(200).json(parkings)
 })
+
+app.put('/parkings/:id', (req,res) => {    
+    const id = parseInt(req.params.id)    
+    let parking = parkings.find(parking => parking.id === id)    
+    parking.name =req.body.name,    
+    parking.city =req.body.city,    
+    parking.type =req.body.type,    
+    res.status(200).json(parking)
+})
+
 app.listen(8080, () => {    
     console.log("Serveur à l'écoute")})
